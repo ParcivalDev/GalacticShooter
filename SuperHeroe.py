716aab470df0507a47e025b3a550f3317efb4735
ParcivalDev <borjarivasdev@gmail.com>
2024-09-08 21:16:04 +0200

    Rectángulo con movimientos básicos

---
+++ SuperHeroe.py
@@ -0,0 +1,61 @@
+import pygame
+import random
+
+# Inicializa todos los módulos de Pygame
+pygame.init()
+
+# Configurar la pantalla
+WIDTH = 800  # ancho
+HEIGHT = 600 # alto
+
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("SuperHeroe")
+
+
+# Colores
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+RED = (255,0,0)
+
+# Jugador
+player_color = WHITE
+player_width = 50
+player_height = 50
+player = pygame.Rect(WIDTH//2 - player_width //2, 
+                     HEIGHT - player_height - 10, 
+                     player_width, 
+                     player_height)
+# Con doble / toma el entero de la división
+# Al ancho de la ventana se le resta el ancho del personaje para que estea en el centro de la ventana
+
+# Reloj para controlar FPS
+clock = pygame.time.Clock()
+
+# Bucle principal del juego para que la ventana no se cierre
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:  # .quit no es un tipo de evento. Es una función
+            print("Cerrando..")
+            running = False
+
+    keys = pygame.key.get_pressed() # Tecla que ha sido presionada
+    if keys[pygame.K_LEFT] and player.left > 10: # Si pongo 0 tocará el borde
+        player.x -= 5
+    if keys[pygame.K_RIGHT] and player.right < WIDTH - 10: # Si pongo 0 tocará el borde
+        player.x += 5
+
+    if keys[pygame.K_UP] and player.top > 10: # Si pongo 0 tocará el borde
+        player.y -= 5
+    if keys[pygame.K_DOWN] and player.bottom < HEIGHT - 10: # Si pongo 0 tocará el borde
+        player.y += 5
+        # Una vez que el borde inferior del jugador toca o supera el límite establecido (HEIGHT - 10), el jugador se detiene y no puede moverse más abajo
+
+    screen.fill(BLACK) # Pinta la pantalla
+    pygame.draw.rect(screen, player_color, player) # Dibuja el jugador como un rectángulo rojo
+    pygame.display.flip() # Actualiza la pantalla
+    clock.tick(60)
+    
+
+
+pygame.quit()
