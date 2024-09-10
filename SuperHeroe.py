b41336e5c230f5972e560364dcaae29fd1d170c0
ParcivalDev <borjarivasdev@gmail.com>
2024-09-10 12:25:58 +0200

    Creación de obstáculos

--- SuperHeroe.py
+++ SuperHeroe.py
@@ -28,6 +28,13 @@ player = pygame.Rect(WIDTH//2 - player_width //2,
 # Con doble / toma el entero de la división
 # Al ancho de la ventana se le resta el ancho del personaje para que estea en el centro de la ventana
 
+
+# Obstáculo
+obstacle_width = 30
+obstacle_height = 30
+obstacle_color = RED
+obstacles = [] # Inicialmente no hay ningún meteorito en la lista
+
 # Reloj para controlar FPS
 clock = pygame.time.Clock()
 
@@ -40,22 +47,43 @@ while running:
             running = False
 
     keys = pygame.key.get_pressed() # Tecla que ha sido presionada
-    if keys[pygame.K_LEFT] and player.left > 10: # Si pongo 0 tocará el borde
+    if (keys[pygame.K_LEFT] or keys[pygame.K_a]) and player.left > 10: # Si pongo 0 tocará el borde. Sin los () se sale del mapa
         player.x -= 5
-    if keys[pygame.K_RIGHT] and player.right < WIDTH - 10: # Si pongo 0 tocará el borde
+    if (keys[pygame.K_RIGHT] or keys[pygame.K_d]) and player.right < WIDTH - 10: # Si pongo 0 tocará el borde
         player.x += 5
 
-    if keys[pygame.K_UP] and player.top > 10: # Si pongo 0 tocará el borde
+    if (keys[pygame.K_UP] or keys[pygame.K_w]) and player.top > 10: # Si pongo 0 tocará el borde superior
         player.y -= 5
-    if keys[pygame.K_DOWN] and player.bottom < HEIGHT - 10: # Si pongo 0 tocará el borde
+    if (keys[pygame.K_DOWN] or keys[pygame.K_s]) and player.bottom < HEIGHT - 10: # Si pongo 0 tocará el borde
         player.y += 5
         # Una vez que el borde inferior del jugador toca o supera el límite establecido (HEIGHT - 10), el jugador se detiene y no puede moverse más abajo
+        
+        
+    # Generación de obstáculos de manera random
+    # Función para generar obstáculos
+    def generate_obstacle():
+        x_position = random.randint(0, WIDTH - obstacle_width)
+        y_position = random.randint(-100, -40)  # Aparecen fuera de la pantalla
+        obstacle_rect = pygame.Rect(x_position, y_position, obstacle_width, obstacle_height)
+        return obstacle_rect
+    
+    # Generar obstáculos (máximo 5)
+    if len(obstacles) < 5:
+        obstacles.append(generate_obstacle())
 
+    # Mover los obstáculos hacia abajo
+    for obstacle in obstacles:
+        obstacle.y += 5  # Velocidad de caída
+        if obstacle.y > HEIGHT:  # Si sale de la pantalla, lo removemos
+            obstacles.remove(obstacle)
+   
+        
     screen.fill(BLACK) # Pinta la pantalla
     pygame.draw.rect(screen, player_color, player) # Dibuja el jugador como un rectángulo rojo
+     # Dibujar los obstáculos
+    for obstacle in obstacles:
+        pygame.draw.rect(screen, obstacle_color, obstacle)
     pygame.display.flip() # Actualiza la pantalla
     clock.tick(60)
-    
-
 
 pygame.quit()
